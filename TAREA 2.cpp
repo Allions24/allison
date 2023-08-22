720684d625553639442b7b0a9e241e14efea8a01
Allions24 <142332557+Allions24@users.noreply.github.com>
2023-08-22 09:55:00 -0600

    Add files via upload

--- TAREA 2.cpp
+++ TAREA 2.cpp
@@ -10,8 +10,8 @@ float temperaturaCelsius ;
 
 temperaturaFahrenheit = (9/5.0)*celsius +32 ;
 temperaturaCelsius = (fahrenheit - 32.0)*(5.0/9.0) ;
-printf ("Mostrar temperaturaFahrenheit: %f\n ", temperaturaFahrenheit);
-printf ("Mostrar temperaturaCelsius: %f\n " , temperaturaCelsius);
+printf ("Mostrar temperatura Celsius en Fahrenheit: %f\n ", temperaturaFahrenheit);
+printf ("Mostrar temperatura Fahrenheit en Celsius: %f\n " , temperaturaCelsius);
    
 	return 0;
 }
